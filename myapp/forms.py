2f59849b5af8d4987be66d5d4127efb5ce756c37
ajay0023130 <ajaybabu0046@gmail.com>
2024-02-07 15:12:43 +0530

    add comment system

---
+++ myapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-contro','placeholder': 'Enter Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'custom-class','placeholder': 'Enter Your Email'}),
+            'body': forms.Textarea(attrs={'class': 'custom-class', 'rows': 5,'placeholder': 'Join the discussion and leave a comment!'}),
+        }
+        required = {
+            'name': True,
+            'email': True,
+            'body': True,
+        }
+       
+        
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Comment
 from django.core.paginator import Paginator, EmptyPage,\
 PageNotAnInteger
+from .forms import  CommentForm
+from django.views.decorators.http import require_POST
+from django.shortcuts import redirect
 # Create your views here.
 
 # this is Post List
@@ -27,4 +30,33 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,status=Post.Status.PUBLISHED,slug=post,publish__year=year,
     publish__month=month,
     publish__day=day)
-    return render(request,'myapp/post/detail.html',{'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    return render(request,'myapp/post/detail.html',{'post': post,
+    'comments': comments,
+    'form': form})
+
+
+# #class based View
+# from django.views.generic import ListView
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 4
+#     template_name = 'myapp/post/list.html'
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comments = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comments = form.save(commit=False)
+        comments.post = post
+        comments.save()
+
+    return redirect('/')
+    # return render(request, 'myapp/comment/comment.html',{'post': post,'comments': comments})
