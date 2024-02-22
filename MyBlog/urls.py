161e5178471d43f319e41cccadc00cef627d53d6
ajay0023130 <ajaybabu0046@gmail.com>
2024-02-22 10:18:00 +0530

    updated code for live

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('', include('myapp.urls', namespace='myapp')),
-path("__debug__/", include("debug_toolbar.urls")),
+
 ]
